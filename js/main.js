1d65aeb2ed1c0c7224a02594e683f75f1950a0f7
Filip <filip.tthuresson@gmail.com>
2021-03-16 09:06:41 +0100

    Small problems

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+var aside = document.querySelector('aside');
+var origOffsetY = aside.offsetTop;
 
+function onScroll(e) {
+  window.scrollY >= origOffsetY ? aside.classList.add('fixed') :
+                                  aside.classList.remove('fixed');
+}
+
+document.addEventListener('scroll', onScroll);
