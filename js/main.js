df0f483c9d13c9e536bbebdfdc3b32fda1e53686
Filip <filip.tthuresson@gmail.com>
2021-03-17 23:01:08 +0100

    Better nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 var aside = document.querySelector('aside');
 var origOffsetY = aside.offsetTop;
-
+var navBTN = document.getElementById("hamburger");
+var navlist = document.getElementById("navList")
 function onScroll(e) {
   window.scrollY >= origOffsetY ? aside.classList.add('fixed') :
                                   aside.classList.remove('fixed');
 }
 
 document.addEventListener('scroll', onScroll);
+
+
+navBTN.addEventListener('click', ()=>{
+  if(navlist.style.right == "-120vw"){
+    navlist.style.right = "0"
+  }else{
+    navlist.style.right = "-120vw"
+  }
+});
