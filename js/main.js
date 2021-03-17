eb5076eb5f6b55934f2ac8167179664f58b0a751
Filip <filip.tthuresson@gmail.com>
2021-03-17 23:30:58 +0100

    Almost complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var aside = document.querySelector('aside');
 var origOffsetY = aside.offsetTop;
 var navBTN = document.getElementById("hamburger");
+
 var navlist = document.getElementById("navList")
 function onScroll(e) {
   window.scrollY >= origOffsetY ? aside.classList.add('fixed') :
@@ -11,9 +12,11 @@ document.addEventListener('scroll', onScroll);
 
 
 navBTN.addEventListener('click', ()=>{
-  if(navlist.style.right == "-120vw"){
-    navlist.style.right = "0"
+  if(navlist.style.right < "-20vw"){
+    navlist.style.right = "-5vw"
+    navBTN.innerHTML ="<h1>X</h1>"
   }else{
-    navlist.style.right = "-120vw"
+    navlist.style.right = "-110vw"
+    navBTN.innerHTML ="<div id='hamburger1'></div><div id='hamburger2'></div><div id='hamburger3'></div>"
   }
 });
